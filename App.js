8ed213fe25cc42dfa70600b5b1984c3a45c712d2
Wilson <swilson@consultant.com>
2022-12-22 01:27:31 +0000

    Update

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, ScrollView, ActivityIndicator } from 'react-native';
+import { TextInput, Button, Card, Title, Paragraph } from 'react-native-paper';
 
-export default function App() {
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const search = () => {
+   // check if searchTerm is not empty
+    if (searchTerm.trim() === '') {
+      setIsLoading(false); // show the loading indicator
+      return; // do not make the API call if searchTerm is empty
+    }
+
+    setIsLoading(true); // show the loading indicator
+    // fetch repos from Github API, limiting the results to 1000
+    fetch(`https://api.github.com/search/repositories?q=${searchTerm}&per_page=1000`)
+      .then(response => response.json())
+      .then(data => {
+        // filter out repos with empty languages
+        const filteredResults = data.items.filter(repo => repo.language !== null && repo.language !== '');
+
+        // group repos by language and count occurrences
+        const groupedResults = filteredResults.reduce((acc, repo) => {
+          if (!acc[repo.language]) {
+            acc[repo.language] = 1;
+          } else {
+            acc[repo.language] += 1;
+          }
+          return acc;
+        }, {});
+
+        // sort languages by occurrence descending
+        const sortedResults = Object.entries(groupedResults).sort((a, b) => b[1] - a[1]);
+
+        setResults(sortedResults);
+        setTotalCount(data.total_count);
+        setIsLoading(false); // hide the loading indicator
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 10, marginTop: 20 }}>
+      <Card style={{ marginBottom: 16 }}>
+        <Card.Content>
+          <TextInput
+            value={searchTerm}
+            onChangeText={text => setSearchTerm(text)}
+            placeholder="Enter search term"
+            style={{ marginBottom: 16 }}
+          />
+          <Button mode="contained" onPress={search}>
+            Go
+          </Button>
+        </Card.Content>
+      </Card>
+      {isLoading ? (
+        // show the loading indicator if isLoading is true
+        <ActivityIndicator />
+      ) : (
+        <ScrollView>
+          {results.map(([language, count]) => (
+            <Card key={language} style={{ marginBottom: 16 }}>
+              <Card.Content>
+                <Title>{language}</Title>
+                <Paragraph>{count} occurrences</Paragraph>
+              </Card.Content>
+            </Card>
+          ))}
+        </ScrollView>
+      )}
+      <Text>{totalCount} total result(s) found</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ readme.md
@@ -0,0 +1,35 @@
+<h3 align="center">Git Search</h3>
+
+<p align="center"> 
+Git Search is a simple application that allows you to search for Github repositories by keyword and displays the results grouped by language.
+</p>
+
+
+## 🎥 Demo / Working <a name = "demo"></a>
+![Working](https://media.giphy.com/media/dYaikM8A2Z01VyZndI/giphy.gif)
+
+Install dependencies
+
+```
+cd git-search
+npm install
+```
+
+Start the development server:
+
+```
+npm run ios
+```
+
+## ⛏️ Built Using <a name = "built_using"></a>
+- React Native
+- React Native Paper
+- Expo
+
+## 🎉 Enhancements
+- Improve user interface
+- Error handling and logging
+- Adding pagination
+- Use functional components and hooks instead of class-based components
+- Add test cases
+- Add debounce to the search input to prevent the API call from being made on every keystroke
